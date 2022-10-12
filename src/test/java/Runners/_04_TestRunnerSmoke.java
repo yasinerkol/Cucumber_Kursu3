cd1ad6d148c7e231ff91244da262ae16e1ab1fd9
yasinerkol <enderimhafsam@gmail.com>
2022-10-12 21:09:07 +0300

    Jenkins Profile build seçenekleri eklendi

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -8,7 +8,8 @@ import io.cucumber.testng.CucumberOptions;
         tags = "@SmokeTest",   // hangi senaryolarda bu etiket varsa onlar çalıştırılacak
         features = {"src/test/java/FeatureFiles"},
         glue = {"StepDefinitions"},
-        plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
+        plugin = {"html:target/cucumber-report.html"}
+       // plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
 )
 
 public class _04_TestRunnerSmoke extends AbstractTestNGCucumberTests {
